924ec2886790828fd1b7670ae53e4636227c4b5e
damyvegaleiva <damyvegaleiva@gmail.com>
2024-09-13 02:59:39 -0500

    Changed limit on Heroes request.

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -3,7 +3,7 @@ const BASE_URL = "https://gateway.marvel.com:443/v1/public/";
 const HEROES_ALL_URL = (offsetValue: number) =>
   `characters?&ts=1&apikey=${import.meta.env.VITE_MARVEL_API_KEY}&hash=${
     import.meta.env.VITE_MARVEL_API_HASH
-  }&limit=25&offset=${offsetValue}`;
+  }&limit=20&offset=${offsetValue}`;
 
 const HEROES_SEARCH_URL = (characterValue: string | undefined) =>
   `characters?name=${characterValue}&ts=1&apikey=${
